6edfaf0059a05dc3372861ad185457a5fc5627d5
twmh99 <twmh14@gmail.com>
2025-06-11 22:03:01 +0700

    9

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -1,20 +1,13 @@
 import EditPage from "@/components/editproduct";
 
-// Tipe parameter halaman
 type PageProps = {
   params: {
     id: string;
   };
 };
 
-// Komponen halaman edit
-export default function PageEdit({ params }: PageProps) {
-  // Convert id dari string ke number jika dibutuhkan
+export default async function PageEdit({ params }: PageProps) {
   const id = parseInt(params.id);
 
-  return (
-    <div className="p-8 max-w-xl mx-auto">
-      <EditPage id={id} />
-    </div>
-  );
+  return <EditPage id={id} />;
 }
