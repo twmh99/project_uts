b2252177f291580d77731dc9a1d146d54fc13196
twmh99 <twmh14@gmail.com>
2025-06-11 22:51:01 +0700

    15

--- app/admin/products/[id]/edit/page.tsx
+++ app/admin/products/[id]/edit/page.tsx
@@ -1,13 +1,19 @@
+// app/admin/products/[id]/edit/page.tsx
 import EditPage from "@/components/editproduct";
+import { notFound } from "next/navigation";
 
-type PageProps = {
+interface PageProps {
   params: {
-    id: any;
+    id: string;
   };
-};
+}
 
 export default function Page({ params }: PageProps) {
   const id = parseInt(params.id);
+
+  if (isNaN(id)) {
+    notFound();
+  }
 
   return (
     <div className="p-8 max-w-xl mx-auto">
